ac2505520ca2d3b8229b2bc5b114ecf070e35694
stemjust4u <stemjust4u@gmail.com>
2021-03-31 07:34:57 -0600

    update comment

--- upython/boot.py
+++ upython/boot.py
@@ -9,18 +9,17 @@ esp.osdebug(None)
 import gc
 gc.collect()
 
-with open("stem32", "rb") as f:
+with open("stem", "rb") as f:
   stem = f.read().splitlines()
-  
 MQTT_SERVER = '10.0.0.115'
-MQTT_USER = stem[0]
-MQTT_PASSWORD = stem[1]
+MQTT_USER = stem[0] 
+MQTT_PASSWORD = stem[1] 
 MQTT_SUB_TOPIC1 = b'esp32/led/instructions'
 MQTT_PUB_TOPIC1 = b'esp32/led/status'
 MQTT_CLIENT_ID = ubinascii.hexlify(machine.unique_id())
 WIFI_SSID = stem[2]
 WIFI_PASSWORD = stem[3]
-
+    
 station = network.WLAN(network.STA_IF)
 
 station.active(True)
