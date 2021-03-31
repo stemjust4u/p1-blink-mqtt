c7a3aab5b4544b181adceef41c3680c2c82c2713
stemjust4u <stemjust4u@gmail.com>
2021-03-31 05:44:38 -0600

    upython

--- upython/main.py
+++ upython/main.py
@@ -1,14 +1,56 @@
-from machine import Pin
-from time import sleep
-#led1 = Pin(5, Pin.OUT)
-led2 = Pin(2, Pin.OUT) #2 is the internal LED
+from machine import Pin, ADC
+from time import time, sleep
+import ujson
 
-#led1.value(0)
-led2.value(1)
+def sub_cb(topic, msg):
+  #print("sub cd function %s %s %s" % (topic, msg, MQTT_SUB_TOPIC1))
+  global newmsg, onoffD
+  if topic == MQTT_SUB_TOPIC1:
+    #print('ESP received message')
+    onoffD = ujson.loads(msg.decode("utf-8", "ignore")) # decode json data to dictionary
+    newmsg = True
+    
+def connect_and_subscribe():
+  global MQTT_CLIENT_ID, MQTT_SERVER, MQTT_SUB_TOPIC1, MQTT_USER, MQTT_PASSWORD
+  client = MQTTClient(MQTT_CLIENT_ID, MQTT_SERVER, user=MQTT_USER, password=MQTT_PASSWORD)
+  client.set_callback(sub_cb)
+  client.connect()
+  client.subscribe(MQTT_SUB_TOPIC1)
+  print('Connected to %s MQTT broker, subscribed to %s topic' % (MQTT_SERVER, MQTT_SUB_TOPIC1))
+  return client
 
+def restart_and_reconnect():
+  print('Failed to connect to MQTT broker. Reconnecting...')
+  sleep(10)
+  machine.reset()
 
+try:
+  client = connect_and_subscribe()
+except OSError as e:
+  restart_and_reconnect()
+
+# MQTT setup is successful. Initialize dictionaries and start the main loop.
+pin = 2
+led = Pin(pin, Pin.OUT) #2 is the internal LED
+ledstatusD = {}
+onoffD = {}
+onoffD["onoff"] = 0
+newmsg = True
 while True:
-    #led1.value(not led1.value())
-    led2.value(not led2.value())
-    print("led2", led2.value())
-    sleep(0.5)
+    try:
+      client.check_msg()
+      if newmsg and onoffD["onoff"] == 1:                     # A new msg turning LED on (on=1)
+        led.value(1)                                         # Turn on LED (set it HIGH)
+        ledstatusD[str(pin) + 'i'] = 1                        # Update LED status for sending via mqtt
+        ledstatusJSON = ujson.dumps(ledstatusD)                # Convert python dictionary to json
+        client.publish(MQTT_PUB_TOPIC1, ledstatusJSON)   # Publish LED status
+        newmsg = False
+      elif newmsg and onoffD["onoff"] == 0:                     # A new msg turning LED off (off=0)
+        led.value(0)
+        ledstatusD[str(pin) + 'i'] = 0
+        ledstatusJSON = ujson.dumps(ledstatusD)
+        client.publish(MQTT_PUB_TOPIC1, ledstatusJSON)
+        newmsg = False
+      sleep(0.1)
+    except OSError as e:
+      restart_and_reconnect()
